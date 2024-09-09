2f1acf12fff69ae91493314ef41e6cdce8c498ae
tripoli3 <tripoli4@naver.com>
2024-09-09 15:15:25 +0900

    second

--- src/App.js
+++ src/App.js
@@ -114,6 +114,7 @@ function App() {
     <>
       <div>
         <Card />
+        <Card />
       </div>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Card = () => {
-  return <div>Card</div>;
+  //logic
+  const subText = '카드 서브 텍스트입니다';
+  const subText2 = '정세헌 카드타이틀';
+  const className = 'chutzrit';
+  //view
+
+  return (
+    <div className="card">
+      <h5 classNme="card-title">{subText2}</h5>
+      <p className={className}>{subText}</p>
+      <button type="button" className="card-button">
+        Go somewhere
+      </button>
+    </div>
+  );
 };
 
 export default Card;
